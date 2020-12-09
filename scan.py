3a0343e2ca1367c6c44a4dcaa9a2ea33b4349e9b
Dmytro Lytovchenko <dmytro.lytovchenko@gmail.com>
2020-12-09 05:41:11 +0100

    Sourcetrail python script calling erlang script

--- scan.py
+++ scan.py
@@ -1,33 +1,55 @@
 #!/usr/bin/env python3
 
 import argparse
+import logging
+import sys
+import traceback
 
 from srctrl_erlang.scanner import Scanner
 
+def setup_logging():
+    logging.basicConfig(filename='sourcetrail-python.log',
+                        filemode="at",
+                        level=logging.DEBUG)
+    return logging.getLogger("scan")
 
-def main():
+LOGGER = setup_logging()
+
+def parse_args():
     parser = argparse.ArgumentParser(description='Parse Erlang files and create Sourcetrail DB')
-
     parser.add_argument('--db', type=str, required=True,
                         help='Path to the Sourcetrail database for this project')
     parser.add_argument('--file', type=str, required=True,
                         help='Path to the Erlang source for this project')
-
+    parser.add_argument('--disasm', type=str, required=False, default="./sourcetrail-disasm",
+                        help='Path to the Escript <sourcetrail-disasm> which will read BEAM files')
     args = parser.parse_args()
+    return args
+
+
+def main():
+    args = parse_args()
 
     db_path = args.db  # type: str
     input_path = args.file  # type: str
+    disasm_path = args.disasm  # type: str
 
     if not input_path.endswith(".beam"):
         # Ignore files other than BEAM
-        return
-    # if not src_path.endswith(".erl") and not src_path.endswith(".hrl"):
-        # Ignore files other than ERL and HRL
-        # return
+        LOGGER.info(f"Ignored file: {input_path} (not a BEAM)")
+        sys.exit(1)
 
-    s = Scanner(db_file=db_path, beam_file=input_path)
-    s.run()
-    s.commit()
+    # if not src_path.endswith(".erl") and not src_path.endswith(".hrl"):
+    # Ignore files other than ERL and HRL
+    # return
+
+    s = Scanner(db_file=db_path, beam_file=input_path, parse_script_path=disasm_path)
+    try:
+        s.run()
+        s.commit()
+    except Exception as e:
+        tb = traceback.format_tb(*sys.exc_info())
+        LOGGER.error(f"ERR {e} tb={tb}")
 
 
 main()
--- srctrl_erlang/scanner.py
+++ srctrl_erlang/scanner.py
@@ -1,56 +1,99 @@
+import json
+import logging
+import os
+import subprocess
 import sys
+from typing import Dict
 
-import sourcetraildb as srctrl
+from srctrl_erlang import sourcetraildb as srctrl
+
+LOGGER = logging.getLogger("libscan")
 
 
 class Scanner:
-    def __init__(self, db_file: str, beam_file: str):
+    def __init__(self, db_file: str, beam_file: str, parse_script_path: str):
+        """ Load AST section from the BEAM file (compile with `+debug_info` !)
+
+        :param parse_script_path: Path to `sourcetrail-disasm` escript (executable)
+        """
+        LOGGER.info(f"Scanning: {beam_file}")
+
         self.beam_file = beam_file
+        self.module_name = os.path.splitext(os.path.basename(self.beam_file))[0]
+        self.ast = load_ast(beam_file, parse_script_path)
 
         if not srctrl.open(db_file):
-            print("ERROR: " + srctrl.getLastError())
-            sys.exit(1)
+            LOGGER.error("File open error: " + srctrl.getLastError())
+            sys.exit(2)
 
-        print(f"Scanning: {self.beam_file}")
+        self.file_id = 0 # type: int
+
+    def run(self):
         srctrl.clear()
         srctrl.beginTransaction()
 
         self.file_id = srctrl.recordFile(self.beam_file)
-
-    def run(self):
         srctrl.recordFileLanguage(self.file_id, "erlang")
+
+        self.record_module(self.module_name)
 
     def commit(self):
         srctrl.commitTransaction()
         if len(srctrl.getLastError()) > 0:
-            print("ERROR: " + srctrl.getLastError())
-            sys.exit(1)
+            LOGGER.error("Sourcetrail commit error: " + srctrl.getLastError())
+            sys.exit(3)
 
         if not srctrl.close():
-            print("ERROR: " + srctrl.getLastError())
-            sys.exit(1)
+            LOGGER.error("Sourcetrail close error: " + srctrl.getLastError())
+            sys.exit(4)
 
     def fail_with(self, msg: str):
-        print(f"ERROR: {msg}")
-        sys.exit(1)
+        LOGGER.error(f"Sourcetrail error: {msg}")
+        sys.exit(6)
 
-    def record_symbol(self, name: str) -> int:
-        symbol_id = srctrl.recordSymbol(
-            '{ "name_delimiter": ".", "name_elements": [ '
-            f'  {"prefix": "", "name": "{name}", "postfix": "" } '
-            '  ] }')
+    def record_module(self, name: str) -> int:
+        LOGGER.info(f"+module: {name}")
+
+        query = {"name_delimiter": ":",
+                 "name_elements": [
+                     {"prefix": "", "name": name, "postfix": ""}
+                 ]}
+        symbol_id = srctrl.recordSymbol(json.dumps(query))
         srctrl.recordSymbolDefinitionKind(symbol_id, srctrl.DEFINITION_EXPLICIT)
-        srctrl.recordSymbolKind(symbol_id, srctrl.SYMBOL_CLASS)
+        srctrl.recordSymbolKind(symbol_id, srctrl.SYMBOL_MODULE)
+        srctrl.recordSymbolLocation(symbol_id, self.file_id, 1, 1, 1, 1)
+        srctrl.recordSymbolScopeLocation(symbol_id, self.file_id, 1, 1, 1, 1)
+        return symbol_id
+
+    def record_function(self, name: str) -> int:
+        query = {"name_delimiter": ":",
+                 "name_elements": [
+                     {"prefix": "", "name": self.module_name, "postfix": ""},
+                     {"prefix": "", "name": name, "postfix": ""}
+                 ]}
+        symbol_id = srctrl.recordSymbol(json.dumps(query))
+        srctrl.recordSymbolDefinitionKind(symbol_id, srctrl.DEFINITION_EXPLICIT)
+        srctrl.recordSymbolKind(symbol_id, srctrl.SYMBOL_FUNCTION)
         srctrl.recordSymbolLocation(symbol_id, self.file_id, 2, 7, 2, 12)
         srctrl.recordSymbolScopeLocation(symbol_id, self.file_id, 2, 1, 7, 1)
         return symbol_id
 
-    def record_member(self, parent_class: str, member_name: str):
-        memberId = srctrl.recordSymbol(
-            '{ "name_delimiter": ".", "name_elements": [ '
-            f'{"prefix": "", "name": "{parent_class}", "postfix": "" }, '
-            f'{"prefix": "", "name": "{member_name}", "postfix": "" } '
-            '] }')
-        srctrl.recordSymbolDefinitionKind(memberId, srctrl.DEFINITION_EXPLICIT)
-        srctrl.recordSymbolKind(memberId, srctrl.SYMBOL_FIELD)
-        srctrl.recordSymbolLocation(memberId, self.file_id, 4, 2, 4, 10)
+    # def record_member(self, parent_class: str, member_name: str):
+    #     query = {"name_delimiter": ":",
+    #              "name_elements": [
+    #                  {"prefix": "", "name": parent_class, "postfix": ""},
+    #                  {"prefix": "", "name": member_name, "postfix": ""}
+    #              ]}
+    #     memberId = srctrl.recordSymbol(json.dumps(query))
+    #     srctrl.recordSymbolDefinitionKind(memberId, srctrl.DEFINITION_EXPLICIT)
+    #     srctrl.recordSymbolKind(memberId, srctrl.SYMBOL_FIELD)
+    #     srctrl.recordSymbolLocation(memberId, self.file_id, 4, 2, 4, 10)
+
+
+def load_ast(beam_file: str, parse_script_path: str) -> Dict:
+    p = subprocess.run([parse_script_path, beam_file],
+                       capture_output=True)
+    if p.returncode == 0:
+        return json.loads(p.stdout)
+    else:
+        raise Exception(f"Disasm tool for {beam_file} failed with:\n{p.stdout}")
